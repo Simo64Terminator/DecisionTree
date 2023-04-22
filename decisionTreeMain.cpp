655787050d979dbcb8d4fb90077bef11875f9dcc
Simo64Terminator <107037742+Simo64Terminator@users.noreply.github.com>
2023-04-22 17:05:02 +0200

    Update decisionTreeMain.cpp
    Updated menu

--- decisionTreeMain.cpp
+++ decisionTreeMain.cpp
@@ -6,43 +6,43 @@ int main(){
 
 	const string menu = "-------------------------------------------------------------------\n\t\t\t\tMENU\n-------------------------------------------------------------------\n1. Read decision tree from file\n2. Insert a labeled node childNode, attached to a node fatherName using an edge edgeLabel\n3. Delete a node\n4. Edit a node\n5. Print current decision tree\n6. Print variables in current tree\n7. Start prediction by inserting values one at a time\n8. Start prediction by inserting all values from start\n0. Exit\n\nChoose ---> ";
 
-	//Definizione variabili interfaccia (ch = menu, l1,l2,la = labelPadre/labelFiglio/labelEdge, ld = delete, le1,le2 = edit, nome_file = nome del .txt usato, lp = labelPrediction)
+	// Defining interface variables (ch = menu | l1,l2,la = fatherLabel,childLabel,edgeLabel | ld = delete | le1,le2 = edit | file_name = name of .txt file used | lp = preditionLabel)
 	choice ch;
 	Label l1, l2, la, ld, le1, le2, lp;
-	string nome_file;
+	string file_name;
 
-	//Mostra Menu
+	// Shows Menu
 	cout << endl << menu;
 	cin >> ch;
 	
-	//Prepara l'albero vuoto
+	// Initialize empty tree
 	Tree tree = d_tree::createEmpty();
 	
 	while(ch != 0){
 		switch(ch){
 			case 1:
-				//Lettura albero da file
+				// Reading tree from file
 				
-				cout << "\nInserisci nome file (richiesto un file di tipo .txt): ";
-				cin >> nome_file;
-				tree = readFromFile(nome_file);
+				cout << "\nInsert file name (.txt file requested): ";
+				cin >> file_name;
+				tree = readFromFile(file_name);
 				if(!isEmpty(tree))
-					cout << "\nOperazione completata\n";
+					cout << "\nSuccess\n";
 				else
-					cout << "\nOperazione fallita\n";
+					cout << "\nFailure\n";
 				cout << "\n";
 				break;
 				
 			case 2:
-				//Inserimento di un nodo etichettato childNode attaccato usando un arco etichettato edgeLabel ad un padre etichettato fatherNode
+				// Insert a labeled node called childNode attached to a father node called fatherNode, by using a labeled edge called edgeLabel
 				
-				cout << "\nInserisci il nome del labelPadre (se l'albero è vuoto inserire $#$#$): ";
+				cout << "\nInsert fatherLabel name (if tree is empty, insert $#$#$): ";
 				cin >> l1;
-				cout << "\nInserisci il nome del labelFiglio da attaccare a " << l1 << ": ";
+				cout << "\nInsert childLabel to be attached to " << l1 << ": ";
 				cin >> l2;
 				if(!isEmpty(tree))
 				{
-					cout << "\nInserisci l'etichetta da assegnare all'arco: ";
+					cout << "\nInsert label to be assigned to the edgeInserisci l'etichetta da assegnare all'arco: ";
 					cin >> la;
 				}
 				else
@@ -50,41 +50,41 @@ int main(){
 				
 				if(!cin)
 				{
-					cout << "Errore inserimento dati\n";
+					cout << "Failure in inserting data\n";
 					break;
 				}
 				
 				removeBlanksAndLower(la);
 				removeBlanksAndLower(l1);
 				removeBlanksAndLower(l2);
-				if(addElem(l1, l2, la, tree) == FAIL){
-					cout << "\nErrore nell'inserimento, operazione annullata\n";
-				}
-				else{
-					cout << "\nOperazione avvenuta con successo!\n";
-				}
+				
+				if(addElem(l1, l2, la, tree) == FAIL)
+					cout << "\nFailure! (inserting error)\n";
+				else
+					cout << "\nSuccess!\n";
+				
 				cout << "\n";
 				break;
 			
 			case 3:
-				//Cancellazione di un nodo dall'albero
+				// Deleting a node
 			
-				cout << "\nInserisci il nome del nodo da eliminare: ";
+				cout << "\nInsert name of node to be deleted: ";
 				cin >> ld;
 				
 				if(!cin)
 				{
-					cout << "Errore inserimento dati\n";
+					cout << "Error in inserting data\n";
 					break;
 				}
 				
 				removeBlanksAndLower(ld);
-				if(deleteElem(ld, tree) == FAIL){
-					cout << "\nErrore nell'eliminazione, operazione annullata\n";
-				}
-				else{
-					cout << "\nOperazione avvenuta con successo!\n";
-				}
+				
+				if(deleteElem(ld, tree) == FAIL)
+					cout << "\nFailure! (deleting error)\n";
+				else
+					cout << "\nSuccess!\n";
+				
 				cout << "\n";
 				break;
 			
